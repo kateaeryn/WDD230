8c8abc58552d4e687eefb4064dd0070e56479d51
kateaeryn <kate.ae.christensen@gmail.com>
2023-01-12 11:52:18 -0700

    update

--- chamber-plan/scripts/getDates.js
+++ chamber-plan/scripts/getDates.js
@@ -1,7 +1,7 @@
 
 let date = new Date().getFullYear();
 
-document.querySelector("#copyright").innerHTML = `&copy;  ${date}  Kate Christensen  Arizona, USA`;
+document.querySelector("#copyright").innerHTML = `&copy;  ${date}  Gilbert | Kate Christensen  | WDD 230 Project`;
 
 let modify = document.lastModified;
 document.querySelector("#lastModified").innerHTML = `Last modified: ${modify}`;
