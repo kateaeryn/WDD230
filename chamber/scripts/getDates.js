22bd44ab8f34dbc354b4319e12268cbc9cfff95a
kateaeryn <kate.ae.christensen@gmail.com>
2023-01-12 17:05:27 -0700

    chamber stage 1

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,7 +1,7 @@
 
 let date = new Date().getFullYear();
 
-document.querySelector("#copyright").innerHTML = `&copy;  ${date}  Gilbert | Kate Christensen  | WDD 230 Project`;
+document.querySelector("#copyright").innerHTML = `&copy;  ${date}  Gilbert Chamber`;
 
 let modify = document.lastModified;
 document.querySelector("#lastModified").innerHTML = `Last modified: ${modify}`;
