52a9a273fface7dfcec4f6629bda6f0694d29378
kateaeryn <kate.ae.christensen@gmail.com>
2023-01-19 15:49:28 -0700

    updates

--- chamber/scripts/pageVisits.js
+++ chamber/scripts/pageVisits.js
@@ -1,20 +1,21 @@
 
 
+//let firstvisit = new Date("Thu Jan 12 2023 15:37:18 GMT-0700 (Mountain Standard Time)");//
 let firstvisit = new Date();
-localStorage.setItem('dateVisited', firstvisit.getTime());
+localStorage.setItem('dateVisited', +firstvisit.getTime());
 
-let start = (localStorage.getItem('set'));
-console.log(start);
+let start = parseInt(localStorage.getItem('dateVisited'));
+
 let end = new Date().getTime();
 
-console.log(end);
+
 
 function getNumberOfDays(start, end) {
     
     const oneDay = 1000 * 60 * 60 * 24;
     
     const diffInTime = end - start;
-    console.log(diffInTime);
+    
     const diffInDays = Math.round(diffInTime / oneDay);
 
     return diffInDays;
@@ -23,7 +24,7 @@ function getNumberOfDays(start, end) {
 
 const lastVisit = document.querySelector(".visits");
 lastVisit.textContent = getNumberOfDays(start, end);
-console.log(lastVisit)
+
 localStorage.setItem('dateVisited', end);
 
 
