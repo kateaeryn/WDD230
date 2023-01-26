56d9ff5dc83e66b906f0891b3bb144361ee84c52
kateaeryn <kate.ae.christensen@gmail.com>
2023-01-26 07:15:45 -0700

    new page

---
+++ chamber/scripts/formdate.js
@@ -0,0 +1,12 @@
+function getDate()
+{
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm}
+    today = yyyy+""+mm+""+dd;
+
+    document.getElementById("dateHidden").value = today;
+}
+getDate();
