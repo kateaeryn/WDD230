fe19a44a84e21e08e1794da075649f0f6f841fc1
kateaeryn <kate.ae.christensen@gmail.com>
2023-01-31 15:36:00 -0700

    json file

--- scripts/learningactivity.js
+++ scripts/learningactivity.js
@@ -1,6 +1,33 @@
 
-const giturl = "https://kateaeryn.github.io/WDD230/index.html"
+const giturl = "https://kateaeryn.github.io/WDD230/learningactivity.json"
 
 
-const links = [fetch('./learningactivity.json').then((response) => response.json()).then((json) => console.table(json))];
+
+
+async function GetActivities() {
+    const response = await fetch(giturl);
+    const links = await response.json();
+    console.table(links);
+    displayLinks(links);
+}
+GetActivities();
+
+const displayLinks = (links) => {
+    links.forEach((week) => {
+        let section = document.createElement('ul');
+        let weekNo = document.createElement('li');
+        let assignment = document.createElement('a');
+               
+        
+        weekNo.innerHTML = `${week[0]}`;
+        assignment.innerHTML = `${week[i]}`;
+
+        
+
+        section.appendChild(weekNo);
+        section.appendChild(assignment);
+        
+      
+    });
+}
 
