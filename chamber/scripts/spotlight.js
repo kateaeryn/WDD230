a9607f6d3a83fc750b3c06a21b4067b799e07cce
kateaeryn <kate.ae.christensen@gmail.com>
2023-02-07 20:48:07 -0700

    yeah

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -3,6 +3,9 @@ const memberUrl = 'https://kateaeryn.github.io/WDD230/chamber/data/members.json'
 
 const cards = document.querySelector('.tier2');
 const array = [];
+let newArray = [];
+let spotNumber = 3;
+var count = 0;
 
 async function GetMemberData() {
     const response = await fetch(memberUrl);
@@ -14,52 +17,53 @@ GetMemberData();
 
 const displaySpotlight = (members) => {
     members.forEach((member) => {
-       
-        if (member.membership === "Silver" || member.membership === "Gold") {
+        
+        if (member.membership == "Gold" || member.membership == "Silver") {
             array.push(member);
-        }
-   
-        const items = 3;
-        let indexTracker = [];
-        let selections = 0;
-        while (selections < items) {
-            let randomIndex = Math.floor(Math.random() * array.length);
-            if (!indexTracker.includes(randomIndex)) {
-                cards.innerHTML = array[randomIndex];
-                selections++;
-                indexTracker.push(randomIndex);
-        
-                console.log(indexTracker);
-                let card = document.createElement('section');
-                let header = document.createElement('h3');
-                let icon = document.createElement('img');
-                let email = document.createElement('a');
-                let phone = document.createElement('p');
-                let website = document.createElement('a');
-                let tagline = document.createElement('p');
+        }   
+        array.forEach((member) => {  
+            if (count < spotNumber) {    
+                let randoMember = array[Math.floor(Math.random() * array.length)];
+                if (!newArray.includes(randoMember)) {
+                    newArray.push(randoMember);
+                
+                    newArray.forEach((member) => {
+                        let card = document.createElement('section');
+                        let header = document.createElement('h3');
+                        let icon = document.createElement('img');
+                        let email = document.createElement('a');
+                        let phone = document.createElement('p');
+                        let website = document.createElement('a');
+                        let tagline = document.createElement('p');
 
-                tagline.innerHTML = `${member.tag}`;
-                email.innerHTML = `${member.email}`;
-                phone.textContent = `${member.phone}`;
-                website.innerHTML = `${member.url}`;
-                website.setAttribute('href', `${member.url}`);
-            
-                icon.setAttribute('src', member.icon);
-                icon.setAttribute('alt', `${member.name}`);
-                icon.setAttribute('loading', 'lazy');
-                icon.setAttribute('width', '200');
-                icon.setAttribute('height', '100');
+                        tagline.innerHTML = `${member.tag}`;
+                        email.innerHTML = `${member.email}`;
+                        phone.textContent = `${member.phone}`;
+                        website.innerHTML = `${member.url}`;
+                        website.setAttribute('href', `${member.url}`);
+                
+                        icon.setAttribute('src', member.icon);
+                        icon.setAttribute('alt', `${member.name}`);
+                        icon.setAttribute('loading', 'lazy');
+                        icon.setAttribute('width', '200');
+                        icon.setAttribute('height', '100');
 
-                header.appendChild(icon);
-                card.appendChild(header);
-                card.appendChild(tagline);
-                card.appendChild(email);
-                card.appendChild(phone);
-                card.appendChild(website);
-                cards.appendChild(card);
-            
-        
-            }
+                        header.appendChild(icon);
+                        card.appendChild(header);
+                        card.appendChild(tagline);
+                        card.appendChild(email);
+                        card.appendChild(phone);
+                        card.appendChild(website);
+                        count++;
+                        cards.appendChild(card);
+                        
+                    })
+}
+            } 
+        } )
         }
-    })
-};
+        
+        )  
+    };  
+        
+console.log(newArray);
